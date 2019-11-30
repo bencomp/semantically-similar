21ec4b7656b10b8453415e5cd0ab24617a0109a8
Ben Companjen <ben@companjen.name>
2019-11-30 05:06:58 +0100

    Implement location and technique endpoints

--- app/main.py
+++ app/main.py
@@ -6,6 +6,12 @@ from SPARQLWrapper import SPARQLWrapper2
 with open('query-nmvw-year.rq') as q_file:
     query_string_year = q_file.read()
 
+with open('query-nmvw-year.rq') as q_file:
+    query_string_year = q_file.read()
+with open('query-nmvw-location.rq') as q_file:
+    query_string_location = q_file.read()
+with open('query-nmvw-technique.rq') as q_file:
+    query_string_technique = q_file.read()
 SPARQL_ENDPOINT_NMVW = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-01/sparql"
 SPARQL_ENDPOINT_RIJKS = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/hackalod/RM-PublicDomainImages/services/RM-PublicDomainImages/sparql"
 
@@ -21,14 +27,42 @@ def year():
         ret = nmvw.query()
     except :
         abort()
-    return jsonify(ret.fullResult)
+    return linked_art_it(uri, ret.bindings)
     # return jsonify([binding["item"].value for binding in ret.bindings])
 
-@app.route('/location/<uri>')
-def location(uri):
-    return "foo based on location"
+@app.route('/location')
+def location():
+    uri = request.args.get('uri', '')
+    nmvw.setQuery(query_string_location.format(uri))
+    try :
+        ret = nmvw.query()
+    except :
+        abort()
+    return linked_art_it(uri, ret.bindings)
 
-@app.route('/technique/<uri>')
-def technique(uri):
-    return "fiep based on technique"
+@app.route('/technique')
+def technique():
+    uri = request.args.get('uri', '')
+    nmvw.setQuery(query_string_technique.format(uri))
+    try :
+        ret = nmvw.query()
+    except :
+        abort()
+    return linked_art_it(uri, ret.bindings)
+
+def linked_art_it(uri, bindings):
+    """Produce Linked Art"""
+    return jsonify([ 
+    {
+        "@context": "https://linked.art/ns/v1/linked-art.json",
+        "id": binding["item"].value,
+        "representation": [
+        {
+            "_label": "NDE LOD",
+            "id": binding["img"].value,
+            "type": "VisualItem"
+        }
+        ],
+        "type": "HumanMadeObject"
+    } for binding in bindings])
 
