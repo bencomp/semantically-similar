3f7068750ba8118ea76997c918f894721c9ef0be
Ben Companjen <ben@companjen.name>
2019-11-30 04:23:55 +0100

    Get full SPARQL results for year

--- app/main.py
+++ app/main.py
@@ -1,30 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 import requests
 app = Flask(__name__)
 
-from SPARQLWrapper import SPARQLWrapper
-with open('query-nmvw.rq') as q_file:
-    queryString = q_file.read()
+from SPARQLWrapper import SPARQLWrapper2
+with open('query-nmvw-year.rq') as q_file:
+    query_string_year = q_file.read()
 
 SPARQL_ENDPOINT_NMVW = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-01/sparql"
 SPARQL_ENDPOINT_RIJKS = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/hackalod/RM-PublicDomainImages/services/RM-PublicDomainImages/sparql"
 
-nmvw = SPARQLWrapper(SPARQL_ENDPOINT_NMVW)
+nmvw = SPARQLWrapper2(SPARQL_ENDPOINT_NMVW)
 # add a default graph, though that can also be part of the query string
 # nmvw.addDefaultGraph("http://www.example.org/graph-selected")
-nmvw.setQuery(queryString)
-try :
-    ret = nmvw.query()
-   # ret is a stream with the results in XML, see <http://www.w3.org/TR/rdf-sparql-XMLres/>
-except :
-#    deal_with_the_exception()
-    pass
 
-
-@app.route('/year/<uri>')
-def year(uri):
-    return queryString.format(uri)
-    # return "bar based on year"
+@app.route('/year')
+def year():
+    uri = request.args.get('uri', '')
+    nmvw.setQuery(query_string_year.format(uri))
+    try :
+        ret = nmvw.query()
+    except :
+        abort()
+    return jsonify(ret.fullResult)
+    # return jsonify([binding["item"].value for binding in ret.bindings])
 
 @app.route('/location/<uri>')
 def location(uri):
